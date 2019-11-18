caa66c6b39be9fb6508e8c6736b2bb5aa42b8058
Liav <liav25@gmail.com>
2019-11-18 19:07:08 +0200

    numberpicker

--- app/src/main/java/com/example/cspingpong/MainActivity.java
+++ app/src/main/java/com/example/cspingpong/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.cspingpong;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NumberPicker hourPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 
 
