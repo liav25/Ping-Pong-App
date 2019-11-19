8c1206774a1201e65a7970f985597d1faa6905a4
avnerduc <avner.sd@gmail.com>
2019-11-19 19:24:26 +0200

    Added an option to join directly to a game, no through server. Added function to get daily and hourly agenda of games. The new methods will create empty games for times where there are no games available.

--- app/src/main/java/com/example/cspingpong/Game.java
+++ app/src/main/java/com/example/cspingpong/Game.java
@@ -1,21 +1,25 @@
 public class Game {
+    private Server serv;
     private int date;
     private int time;
     private String player1;
     private String player2;
 
-    public Game(int date, int time){
+    public Game(Server server, int date, int time){
+        this.serv = server;
         this.date = date;
         this.time = time;
     }
 
-    public Game(int date, int time, String player1){
+    public Game(Server server, int date, int time, String player1){
+        this.serv = server;
         this.date = date;
         this.time = time;
         this.player1 = player1;
     }
 
-    public Game(int date, int time, String player1, String player2){
+    public Game(Server server, int date, int time, String player1, String player2){
+        this.serv = server;
         this.date = date;
         this.time = time;
         this.player1 = player1;
@@ -34,13 +38,9 @@ public class Game {
 
     public String getPlayer2(){ return this.player2; }
 
-    public boolean isFull(){
-        return !this.player1.equals("") && !this.player2.equals("");
-    }
+    public boolean isFull(){ return this.player1 != null && this.player2 != null; }
 
-    public boolean isEmpty(){
-        return this.player1.equals("") && this.player2.equals("");
-    }
+    public boolean isEmpty(){ return this.player1 == null && this.player2 == null; }
 
     public int empty_slots(){
         if(this.isFull()){
@@ -50,6 +50,12 @@ public class Game {
         } return 1;
     }
 
+    @Override
+    public String toString() {
+        return "Game Object. Date: "+this.getDate()+" Time: "+this.getTime()+
+                " Player1: "+this.getPlayer1()+" Player2: "+this.getPlayer2();
+    }
+
     /**
      * Method for adding a player to the game
      * @param player - The name of the player to add
@@ -57,11 +63,14 @@ public class Game {
      * or the game is already full
      */
     public boolean addPlayer(String player){
-        if (player == null || player.equals(this.player1) || player.equals(this.player2)){
+        if (player == null || player.equals(this.player1) || player.equals(this.player2)) {
             return false;
         }
         if(this.player1 == null){
             this.player1 = player;
+            if(this.player2 == null){
+                this.serv.addGame(this);
+            }
         } else if(this.player2 == null){
             this.player2 = player;
         } else return false;
--- app/src/main/java/com/example/cspingpong/Server.java
+++ app/src/main/java/com/example/cspingpong/Server.java
@@ -6,22 +6,24 @@ import java.util.stream.IntStream;
 
 public class Server {
 
-    private ArrayList<Game> gamelist;
+    private ArrayList<Game> game_list;
     private int INTERVAL = 100;
-    private int HOUR = 60;
-    private int SLOT_TIME = 15;
-    //private Gson gson;
+    private int MINUTES_IN_HOUR = 60;
+    private int HOURS_IN_DAY = 24;
+    private int SLOT_TIME = 15; //Number of minutes for each slot. make sure it divides 60
     private String json;
+    //private Gson gson;
 
-    public Server(){
+    Server(){
         //this.gson = new Gson();
-        gamelist = new ArrayList<Game>();
+        game_list = new ArrayList<>();
         //init gamelist from json
     }
 
-    public ArrayList<Game> get_games_by_date(int date){
-        ArrayList<Game> games_by_date = new ArrayList<Game>();
-        for (Game g : gamelist){
+    // Deprecated xD ----> I mean that "frontend" might prefer working with get_day_agenda instead
+    ArrayList<Game> get_games_by_date(int date){
+        ArrayList<Game> games_by_date = new ArrayList<>();
+        for (Game g : game_list){
             if(g.getDate() == date){
                 games_by_date.add(g);
             }
@@ -29,23 +31,36 @@ public class Server {
         return games_by_date;
     }
 
-    //TODO: rename function to get_game_slots
-    public ArrayList<Game> get_games_by_date_and_time(int date, int hour){
-        ArrayList<Game> game_slots = new ArrayList<Game>();
-        List<Integer> range = IntStream.rangeClosed(0, (HOUR  / SLOT_TIME) - 1)
+    ArrayList<Game> get_hour_agenda(int date, int hour){
+        ArrayList<Game> game_slots = new ArrayList<>();
+        List<Integer> range = IntStream.rangeClosed(0, (MINUTES_IN_HOUR / SLOT_TIME) - 1)
                 .boxed().collect(Collectors.toList());
-        for (int i : range){
+        for (int slot : range){
             boolean flag = false;
-            for (Game g : gamelist){
-                if(g.getDate() == date && hour + (SLOT_TIME * i) == g.getTime()){
+            for (Game g : game_list){
+                if(g.getDate() == date && hour + (SLOT_TIME * slot) == g.getTime()){
                     game_slots.add(g);
                     flag = true;
                     break;
                 }
             }
             if (!flag){
-                game_slots.add(new Game(date, hour + (SLOT_TIME * i)));
+                game_slots.add(new Game(this, date, hour + (SLOT_TIME * slot)));
             }
+        }
+        return game_slots;
+    }
+
+    /**
+     * A method that returns a full day's games
+     * @param date the date to return games of
+     * @return ArrayList of games of every time slot in the day
+     */
+    ArrayList<Game> get_day_agenda(int date){
+        ArrayList<Game> game_slots = new ArrayList<>();
+        List<Integer> hour_range = IntStream.rangeClosed(0, HOURS_IN_DAY - 1).boxed().collect(Collectors.toList());
+        for (int hour : hour_range){
+            game_slots.addAll(get_hour_agenda(date, hour * INTERVAL));
         }
         return game_slots;
     }
@@ -56,14 +71,18 @@ public class Server {
      * @param date the date of the game in 8 digits, as in 22122019
      * @param time the time of the game in 4 digits, as in 1215
      * @param user the username
-     * @return
+     * @return whether the player was successfully added to the game
      */
-    public boolean join(int date, int time, String user){
-        for (Game g : gamelist){
+    boolean join(int date, int time, String user){
+        for (Game g : game_list){
             if(g.getDate() == date && g.getTime() == time){
                 return g.addPlayer(user);
             }
-        } return gamelist.add(new Game(date, time, user));
+        } return game_list.add(new Game(this, date, time, user));
+    }
+
+    void addGame(Game game){
+        game_list.add(game);
     }
 
     public void load_state(){
@@ -76,6 +95,6 @@ public class Server {
     }
 
     public void reset(){
-        this.gamelist = new ArrayList<Game>();
+        this.game_list = new ArrayList<>();
     }
 }
